15fa761d9948a154b4a22305db2789a2de9792bb
JackkyyH <30927793+jackkyyh@users.noreply.github.com>
2024-02-03 21:43:15 +0800

    rename my-dot-files to dot-files

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
 # Install theme
-ln -s ~/my-dot-files/custom.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/custom.zsh-theme
+ln -s ~/dot-files/custom.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/custom.zsh-theme
 
 # Install plugins
 cd ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/
@@ -15,15 +15,15 @@ fi
 
 # Install zshrc
 mv ~/.zshrc ~/.zshrc_bak
-ln -s ~/my-dot-files/zshrc ~/.zshrc
+ln -s ~/dot-files/zshrc ~/.zshrc
 
 # Install custom.zsh
-ln -s ~/my-dot-files/custom.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/custom.zsh
+ln -s ~/dot-files/custom.zsh ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/custom.zsh
 
 
 
 # rm -f ~/.vimrc ~/.zshrc ~/.myrc ~/.tmux.conf
 # for FILE in vimrc zshrc myrc tmux.conf
 # do
-# 	ln -s ~/my-dot-files/$FILE ~/.$FILE
+# 	ln -s ~/dot-files/$FILE ~/.$FILE
 # done

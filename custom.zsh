aa574a0d73a255aef3a8c7c17f68320f1d1e9f26
JackkyyH <30927793+jackkyyh@users.noreply.github.com>
2024-04-02 14:30:04 +0800

    Update proxy alias in custom.zsh

--- custom.zsh
+++ custom.zsh
@@ -5,8 +5,8 @@ if [[ "$HOST" = *"gpugate1" || "$HOST" = *"gpu2gate1" ]]; then
     alias gpu-interactive="srun --gres=gpu:1 --cpus-per-task=4 --pty sh -c 'echo Node ip: \$(hostname -I); zsh'"
     alias gpu-interactive-3090="srun -p q3090 --gres=gpu:rtx3090:2 --cpus-per-task=8 --pty sh -c 'echo Node ip: \$(hostname -I); zsh'"
 elif [[ "$OSTYPE" = "darwin"* ]]; then
-    alias proxy="export ALL_PROXY=socks5://127.0.0.1:7890"
-    alias unproxy="unset all_proxy"
+    alias enable_proxy="export ALL_PROXY=socks5://127.0.0.1:7890"
+    alias disable_proxy="unset ALL_PROXY"
     # export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.aliyun.com/homebrew/homebrew-bottles
 fi
 
